2569a4d7c7a27ac4395bc285cbb7a016779d8dbb
MichalZdziubany <127780743+MichalZdziubany@users.noreply.github.com>
2025-03-21 19:42:03 +0000

    Added read from file function

--- Lz77.c
+++ Lz77.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-void compress(char* inputfile, char* outputfile){
-    printf("Compressing %s to %s\n", inputfile, outputfile);
+void compress(char* inputText, char* outputfile){
+    printf("Compressing to %s\n", outputfile);
 }
 
-void decompress(char* inputfile, char* outputfile){
-    printf("DeCompressing %s to %s\n", inputfile, outputfile);
+void decompress(char* inputText, char* outputfile){
+    printf("DeCompressing to %s\n", outputfile);
+}
+
+char* readFile(char* filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Could not open file %s\n", filename);
+        exit(1);
+    }
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    fread(buffer, 1, fileSize, file);
+    buffer[fileSize] = '\0';
+    fclose(file);
+    return buffer;
 }
 
 int main(){
@@ -24,14 +44,18 @@ int main(){
         scanf("%s", inputFile);
         printf("Enter the name of the output file: ");
         scanf("%s", outputFile);
-        compress(inputFile, outputFile);
+        char* inputText = readFile(inputFile);
+        compress(inputText, outputFile);
+        free(inputText);
     } else if (option == 2)
     {
         printf("Enter the name of the file to decompress: ");
         scanf("%s", inputFile); 
         printf("Enter the name of the output file: ");
         scanf("%s", outputFile);
-        decompress(inputFile, outputFile);
+        char* inputText = readFile(inputFile);
+        decompress(inputText, outputFile);
+        free(inputText);
     }else{
         printf("Invalid option\n");
 
